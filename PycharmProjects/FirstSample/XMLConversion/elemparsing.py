aa5eff11db78252a2ea45140fdedc82f070a8ad6
Anuragh27crony <Anuragh27crony@gmail.com>
2016-09-06 00:03:07 +0200

    refactoring 3

--- PycharmProjects/FirstSample/XMLConversion/elemparsing.py
+++ PycharmProjects/FirstSample/XMLConversion/elemparsing.py
@@ -7,16 +7,34 @@ feature_list = []
 def _another():
     source = 'XMLFiles/2016-09-01_161608_output_4.3_Rerun.xml'
     for event, elem in ET.iterparse(source=source, events=('start', 'end')):
+        has_test_tag = False
+        test_case = None
+        suite_count=1
+        story = None
+
+        # TODO: First suite is feature
+        # TODO: Append Name from Second suite till test case for Story
         if event == 'start':
             print("start: ", elem.tag, elem.attrib)
+            if elem.tag == 'test':
+                has_test_tag = True
+                # TODO: Append name from TestCase till last keyword
+                test_case = TestCase(elem.key('name'))
         if event == 'end':
             print("End: ", elem.tag, elem.attrib)
             if elem.tag == 'kw':
                 current_step = _prepare_step(elem)
                 print("====>")
                 print(current_step)
+                if has_test_tag:
+                    test_case.add_step(current_step)
                 print("<====>")
                 elem.clear()
+            if elem.tag == 'test':
+                pass
+
+            if elem.tag == 'suite':
+                pass
 
 
 def _prepare_step(keyword_elem):
@@ -29,6 +47,14 @@ def _prepare_step(keyword_elem):
 
     new_step = Step(step_name, step_status, step_start_time, step_end_time)
     return new_step
+
+
+def _prepare_test_case(testcase_elem):
+    pass
+
+
+def _prepare_story(story_elem):
+    pass
 
 
 _another()
