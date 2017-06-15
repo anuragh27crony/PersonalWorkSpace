5760e99f3fdc67bfaaf0ff2a70644792769b1935
Anurag Mala <anurag.mala@4cinsights.com>
2017-06-15 17:18:52 +0200

    updated logic

--- PycharmProjects/DownloadManager/Api/PushDisconnectedData.py
+++ PycharmProjects/DownloadManager/Api/PushDisconnectedData.py
@@ -4,28 +4,57 @@ from datetime import datetime
 import requests
 import json
 
+dwld_mgr_url = "http://detector.teletrax.com/v1/prtg"
+accept_headers = {"Accept": "application/json"}
 
-def fetch_and_push_data():
-    detectors_set = set()
-    dwld_mgr_url = "http://detector.teletrax.com/v1/prtg/channels"
-    headers = {"Accept": "application/json"}
-    time_stamp = datetime.utcnow().isoformat()
+
+def fetch_connected_detectors():
+    fetch_detectors = dwld_mgr_url + "/detectors"
+    connected_detectors_list = list()
     try:
-        response = requests.request("GET", dwld_mgr_url, headers=headers)
+        response = requests.request("GET", fetch_detectors, headers=accept_headers)
+        if response.status_code is not 200:
+            print(response.status_code)
+        else:
+            channel_data = json.loads(response.text)
+            for node_wise_detectors in channel_data:
+                connected_detectors_list.extend(node_wise_detectors.get("detectors"))
+    except Exception as e:
+        print(e)
+    return connected_detectors_list
+
+
+def clean_disconnected_detectors(disconnected_detectors_set, connected_detectors_list):
+    result_disconnected_detectors = disconnected_detectors_set.copy()
+    for disconnected_detector in disconnected_detectors_set:
+        if disconnected_detector in connected_detectors_list:
+            result_disconnected_detectors.remove(disconnected_detector)
+    print(result_disconnected_detectors)
+    return result_disconnected_detectors
+
+
+def fetch_disconnected_detectors():
+    disconnected_detectors_set = set()
+    fetch_channels = dwld_mgr_url + "/channels"
+
+    try:
+        response = requests.request("GET", fetch_channels, headers=accept_headers)
         if response.status_code is not 200:
             print(response.status_code)
         else:
             channel_data = json.loads(response.text)
             for channel in channel_data.get("configured"):
-                detectors_set.add(channel.get("detectorName"))
+                disconnected_detectors_set.add(channel.get("detectorName"))
 
-            parse_detectors_data(detectors_set, time_stamp)
+
     except Exception as e:
         print(e)
+    return disconnected_detectors_set
 
 
-def parse_detectors_data(detectors_set, time_stamp):
-    for detector in detectors_set:
+def parse_push_data_kibana(detectors_set, time_stamp):
+    cleaned__disconnected_detectors_set = clean_disconnected_detectors(detectors_set, fetch_connected_detectors())
+    for detector in cleaned__disconnected_detectors_set:
         insert_data = {"detectorId": detector, "timestamp": time_stamp}
         push_elastic_search(insert_data)
 
@@ -45,7 +74,11 @@ def initiate():
     push_frequency_in_secs = 60
     while True:
         start_time = time.time()
-        fetch_and_push_data()
+
+        time_stamp = datetime.utcnow().isoformat()
+        disconnected_detectors_set = fetch_disconnected_detectors()
+        parse_push_data_kibana(disconnected_detectors_set, time_stamp)
+
         exec_time_diff = time.time() - start_time
         if exec_time_diff < push_frequency_in_secs:
             time.sleep(push_frequency_in_secs - exec_time_diff)
